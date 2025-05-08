cfae284150a557296a3498be425be674c87ba766
gfinol <gerard.finol@urv.cat>
2025-05-08 13:21:09 +0100

    Add geospatial preprocessing

--- vllm/model_executor/models/prithvi_geospatial_mae.py
+++ vllm/model_executor/models/prithvi_geospatial_mae.py
@@ -15,12 +15,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Inference-only IBM/NASA Prithvi Geospatial model."""
+import copy
+import dict
+import importlib
 from collections.abc import Iterable, Mapping, Sequence
 from typing import Optional, Set, Tuple, Union
 
+import albumentations
+import numpy as np
 import torch
 import torch.nn as nn
+from einops import rearrange
 from transformers import BatchFeature
+from terratorch.datamodules import Sen1Floods11NonGeoDataModule
+
 
 from vllm.config import VllmConfig
 from vllm.model_executor.model_loader.weight_utils import default_weight_loader
@@ -86,6 +94,117 @@ class PrithviGeoSpatialMAEMultiModalProcessor(BaseMultiModalProcessor):
     ) -> Sequence[PromptUpdate]:
         return []
 
+    def _preprocess(self, x: np.ndarray, img_size, location_coords, datamodule):
+        # Reflect pad if not divisible by img_size
+        original_h, original_w = x.shape[-2:]
+        pad_h = (img_size - (original_h % img_size)) % img_size
+        pad_w = (img_size - (original_w % img_size)) % img_size
+        x = np.pad(
+            x, ((0, 0), (0, 0), (0, 0), (0, pad_h), (0, pad_w)), mode="reflect"
+        )
+
+        # Build sliding window
+
+        batch_size = 1
+        batch = torch.tensor(x, device="cpu")
+        windows = batch.unfold(3, img_size, img_size).unfold(4, img_size, img_size)
+        h1, w1 = windows.shape[3:5]
+        windows = rearrange(
+            windows, "b c t h1 w1 h w -> (b h1 w1) c t h w", h=img_size, w=img_size
+        )
+
+        # Split into batches if number of windows > batch_size
+        num_batches = windows.shape[0] // batch_size if windows.shape[0] > batch_size else 1
+        windows = torch.tensor_split(windows, num_batches, dim=0)
+
+        # location_coords = location_coords[0]
+
+        # TODO - Add support for more than one window
+        for x in windows[:1]:
+            # Apply standardization
+            x = datamodule.test_transform(image=x.squeeze().numpy().transpose(1, 2, 0))
+            x = datamodule.aug(x)['image']
+
+            mm_data = {
+                "pixel_values": torch.empty(0) if x is None else x,
+                "location_coords": torch.empty(0) if location_coords is None else location_coords,
+                "img_size": img_size,
+                "h1": h1,
+                "w1": w1,
+                "original_h": original_h,
+                "original_w": original_w,
+            }
+            return mm_data
+
+        return None
+
+    def init_object_from_classpath_and_args(self, class_name: str, init_args: dict):
+        class_parts = class_name.split('.')
+        module_name = '.'.join(class_parts[:-1])
+        class_name = class_parts[-1]
+
+        # Import the module
+        module = importlib.import_module(module_name)
+
+        # Get the class
+        cls = getattr(module, class_name)
+
+        return cls(**init_args)
+
+    def parse_args_list(self, args: list):
+        return [
+            self.init_object_from_classpath_and_args(t["class_path"], t["init_args"])
+            for t in args]
+
+    def generate_datamodule(self, classpath: str, init_args: dict):
+
+        final_init_args = copy.deepcopy(init_args)
+
+        final_init_args["test_transform"] = self.parse_args_list(init_args["test_transform"])
+        final_init_args["val_transform"] = self.parse_args_list(init_args["val_transform"])
+        final_init_args["train_transform"] = self.parse_args_list(init_args["test_transform"])
+
+        datamodule = self.init_object_from_classpath_and_args(classpath, final_init_args)
+        return datamodule
+
+    @staticmethod
+    def generate_datamodule_static():
+        datamodule_config = {
+            'bands': ['BLUE',
+                      'GREEN',
+                      'RED',
+                      'NIR_NARROW',
+                      'SWIR_1',
+                      'SWIR_2'],
+            'batch_size': 16,
+            'constant_scale': 0.0001,
+            'data_root': '/dccstor/geofm-finetuning/datasets/sen1floods11',
+            'drop_last': True,
+            'no_data_replace': 0.0,
+            'no_label_replace': -1,
+            'num_workers': 8,
+            'test_transform': [albumentations.Resize(always_apply=False,
+                                                     height=448,
+                                                     interpolation=1,
+                                                     p=1,
+                                                     width=448),
+                               albumentations.pytorch.ToTensorV2(
+                                   transpose_mask=False,
+                                   always_apply=True,
+                                   p=1.0
+                               )],
+        }
+
+        datamodule = Sen1Floods11NonGeoDataModule(data_root=datamodule_config['data_root'],
+                                                  batch_size=datamodule_config["batch_size"],
+                                                  num_workers=datamodule_config["num_workers"],
+                                                  bands=datamodule_config["bands"],
+                                                  drop_last=datamodule_config["drop_last"],
+                                                  test_transform=datamodule_config["test_transform"
+                                                                                   ""])
+
+        return datamodule
+
     def apply(
         self,
         prompt: Union[str, list[int]],
@@ -93,10 +212,19 @@ class PrithviGeoSpatialMAEMultiModalProcessor(BaseMultiModalProcessor):
         hf_processor_mm_kwargs: Mapping[str, object],
         return_mm_hashes: bool = False,
     ) -> MultiModalInputs:
-        mm_kwargs = {}
 
-        for k, v in mm_data.items():
-            mm_kwargs[k] = v
+        config = {} # TODO load config from somewhere
+        config = self.info
+        input_data = mm_data["input_data"]
+        # temporal_coords = mm_data["temporal_coords"]
+        location_coords = mm_data["location_coords"]
+        # datamodule = self.generate_datamodule(config["data"]["class_path"], config["data"]["init_args"])
+        datamodule = self.generate_datamodule_static()
+        mm_kwargs = self._preprocess(input_data, 512, location_coords, datamodule)
+
+        # mm_kwargs = {}
+        # for k, v in mm_data.items():
+        #     mm_kwargs[k] = v
 
         return MultiModalInputs(
             type="multimodal",
@@ -183,10 +311,47 @@ class PrithviGeoSpatialMAE(nn.Module, IsAttentionFree, SupportsMultiModal,
 
         pixel_values, location_coords = (
             self._parse_and_validate_multimodal_data(**kwargs))
+
+        img_size = kwargs.pop("img_size", 512)
+        h1 = kwargs.pop("h1", 1)
+        w1 = kwargs.pop("w1", 1)
+        original_h = kwargs.pop("original_h", 512)
+        original_w = kwargs.pop("original_w", 512)
+
         model_output = self.model(pixel_values,
                                   location_coords=location_coords)
 
-        return model_output.output
+        pred_imgs = []
+
+        pred = model_output.output
+
+        y_hat = pred.argmax(dim=1)
+
+        y_hat = torch.nn.functional.interpolate(y_hat.unsqueeze(1).float(), size=img_size, mode="nearest")
+
+        pred_imgs.append(y_hat)
+
+        pred_imgs = torch.concat(pred_imgs, dim=0)
+
+        # Build images from patches
+        pred_imgs = rearrange(
+            pred_imgs,
+            "(b h1 w1) c h w -> b c (h1 h) (w1 w)",
+            h=img_size,
+            w=img_size,
+            b=1,
+            c=1,
+            h1=h1,
+            w1=w1,
+        )
+
+        # Cut padded area back to original size
+        pred_imgs = pred_imgs[..., :original_h, :original_w]
+
+        # Squeeze (batch size 1)
+        pred_imgs = pred_imgs[0]
+
+        return pred_imgs
 
     def pooler(
         self,
